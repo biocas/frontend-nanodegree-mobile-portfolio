7df8d71778e48747b8d1b8e5e6423b8d8589e160
Margarida Salvado <margaridasalvado@MacBook-Pro-de-Margarida.local>
2017-10-18 16:58:31 +1100

    replace with getElementById in changeSliderLabel function

--- views/js/main.js
+++ views/js/main.js
@@ -407,13 +407,13 @@ var resizePizzas = function(size) {
   function changeSliderLabel(size) {
     switch(size) {
       case "1":
-        document.querySelector("#pizzaSize").innerHTML = "Small";
+        document.getElementById("pizzaSize").innerHTML = "Small";
         return 25;
       case "2":
-        document.querySelector("#pizzaSize").innerHTML = "Medium";
+        document.getElementById("pizzaSize").innerHTML = "Medium";
         return 33.3;
       case "3":
-        document.querySelector("#pizzaSize").innerHTML = "Large";
+        document.getElementById("pizzaSize").innerHTML = "Large";
         return 50;
       default:
         console.log("bug in changeSliderLabel");
